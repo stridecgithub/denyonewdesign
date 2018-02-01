8d55c46251c39b9dfafeb51aff4a718ceb9bd5b3
Kannan Nagarathinam <kannan@webneo.in>
2018-02-01 16:34:26 +0530

    Ui changed for compose icon

---
+++ src/pages/piclocation/piclocation.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PiclocationPage } from './piclocation';
+
+@NgModule({
+  declarations: [
+    PiclocationPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(PiclocationPage),
+  ],
+})
+export class PiclocationPageModule {}
