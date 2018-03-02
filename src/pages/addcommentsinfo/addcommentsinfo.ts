3002629cb88cef1e8f5c9a7623634965e00893ff
Vinoth <vino@stridec.com>
2018-03-02 20:16:44 +0530

    ui border and @mention reverted

--- src/pages/addcommentsinfo/addcommentsinfo.ts
+++ src/pages/addcommentsinfo/addcommentsinfo.ts
@@ -14,10 +14,7 @@ import { NotificationPage } from '../notification/notification';
 import { Config } from '../../config/config';
 declare var jQuery: any;
 declare var mention: any;
-declare var $container: any;
-declare var $backdrop: any;
-declare var $highlights: any;
-declare var $textarea: any;
+
 /**
  * Generated class for the addhocPage page.
  *
@@ -154,67 +151,7 @@ export class AddcommentsinfoPage {
   ionViewDidLoad() {
     //this.tabBarElement.style.display = 'none';
 	
-	var $container = $('.container');
-	var $backdrop = $('.backdrop');
-	var $highlights = $('.highlights');
-	var $textarea = $('textarea');
-	
-	
-	// yeah, browser sniffing sucks, but there are browser-specific quirks to handle that are not a matter of feature detection
-	var ua = window.navigator.userAgent.toLowerCase();
-	var isIE = !!ua.match(/msie|trident\/7|edge/);
-	var isWinPhone = ua.indexOf('windows phone') !== -1;
-	var isIOS = !isWinPhone && !!ua.match(/ipad|iphone|ipod/);
-	
-	function applyHighlights(text) {
-	  text = text
-		.replace(/\n$/g, '\n\n')
-		.replace(/@([^ ]+)/g, '<mark>$&</mark>');
-	  
-	  if (isIE) {
-		// IE wraps whitespace differently in a div vs textarea, this fixes it
-		text = text.replace(/ /g, ' <wbr>');
-	  }
-	  
-	  return text;
-	}
-	
-	function handleInput() {
-	  var text = $textarea.val();
-	  var highlightedText = applyHighlights(text);
-	  $highlights.html(highlightedText);
-	}
-	
-	function handleScroll() {
-	  var scrollTop = $textarea.scrollTop();
-	  $backdrop.scrollTop(scrollTop);
-	  
-	  var scrollLeft = $textarea.scrollLeft();
-	  $backdrop.scrollLeft(scrollLeft);  
-	}
-	
-	function fixIOS() {
-	  // iOS adds 3px of (unremovable) padding to the left and right of a textarea, so adjust highlights div to match
-	  $highlights.css({
-		'padding-left': '+=3px',
-		'padding-right': '+=3px'
-	  });
-	}
-	
-	function bindEvents() {
-	  $textarea.on({
-		'input': handleInput,
-		'scroll': handleScroll
-	  });
-	
-	}
-	
-	if (isIOS) {
-	  fixIOS();
-	}
-	
-	bindEvents();
-	handleInput();
+
 
     let already = localStorage.getItem("microtime");
     if (already != undefined && already != 'undefined' && already != '') {
